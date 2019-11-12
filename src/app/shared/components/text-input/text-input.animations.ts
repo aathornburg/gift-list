b9a2290a22251ed3c4018a11e2828d9b1fd25339
Alan Thornburg <alan.thornb@gmail.com>
2019-11-12 17:25:21 -0500

    Changed text inputs back to not using ion item. Still need to do research

--- src/app/shared/components/text-input/text-input.animations.ts
+++ src/app/shared/components/text-input/text-input.animations.ts
@@ -1,9 +1,10 @@
 import { trigger, state, style, animate, transition, query, animateChild, group } from '@angular/animations';
 
-const labelHeight = '12px'
+const labelHeight = '14px'
+const heightFromTop = '7px';
 
 export const moveUpShrink = trigger('moveUpShrink', [
-    state('true', style({top: '6px', 'font-size': labelHeight, height: labelHeight})),
+    state('true', style({top: heightFromTop, 'font-size': labelHeight, height: labelHeight, color: '#b6eae5', 'font-weight': 'bold'})),
     transition('false => true', [
         animate(`150ms ease-in-out`)
     ]),
