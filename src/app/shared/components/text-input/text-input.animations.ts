42f4bda6662fbfe4635dd7be3f34cce46954ece9
Alan Thornburg <alan.thornb@gmail.com>
2019-11-14 09:22:52 -0500

    Updated text input component away from ion-item but mirroring most of its features

--- src/app/shared/components/text-input/text-input.animations.ts
+++ src/app/shared/components/text-input/text-input.animations.ts
@@ -4,7 +4,7 @@ const labelHeight = '14px'
 const heightFromTop = '7px';
 
 export const moveUpShrink = trigger('moveUpShrink', [
-    state('true', style({top: heightFromTop, 'font-size': labelHeight, height: labelHeight, color: '#b6eae5', 'font-weight': 'bold'})),
+    state('true', style({top: heightFromTop, 'font-size': labelHeight, height: labelHeight, color: '#2EC4B6'})),
     transition('false => true', [
         animate(`150ms ease-in-out`)
     ]),
